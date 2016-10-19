d022182d7dc62312f76445b1c39cb122ebe4b23a
gericsapo <geri@h369.aau1x.klient.sydhavn.site.aau.dk>
2016-10-19 16:27:07 +0200

    little fix

--- centroid.m
+++ centroid.m
@@ -3,13 +3,12 @@ function C = centroid(x)
 
     X = fft(x); %fast fourier transform
     X_mag = abs(X); %magnitude spectrum
-    X_mag = X_mag(1:length(X_mag)/2);
-   
-    C = X_mag*(0:length(X_mag)-1)'/sum(X_mag);
-    
-    %w = [1:length(x)].*fs/length(x); %center frequency of each bin
+    X_mag = X_mag(1:length(X_mag)/2); %cropping the n egative region
+    w = [0:length(X_mag)-1] %vector of bins where 0 corresponds to DC component which needs to be excluded
 
-    %C = sum(w(1:length(w)/2).*X_mag(1:length(X_mag)/2))/sum(X_mag(1:length(X_mag)/2));
+    C = X_mag * w'/sum(X_mag); % centroid FUCK YEAH
+    
+    %C_hz = C * fs/length(w) 
 
 end
 
