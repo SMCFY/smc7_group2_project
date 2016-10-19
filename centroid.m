c597781b41088369d88015fab6cbfd0421a1844a
NikolajAndersson <nikolajandersson@gmail.com>
2016-10-19 16:02:47 +0200

    transposed the mySignal, now it works

--- centroid.m
+++ centroid.m
@@ -1,11 +1,11 @@
 function C = centroid(x)
 %SPECTRAL CENTROID calculates the spectral centorid of a discrete signal
 
-    X = fft(x); %fas fourier transform
+    X = fft(x); %fast fourier transform
     X_mag = abs(X); %magnitude spectrum
     X_mag = X_mag(1:length(X_mag)/2);
    
-    C = (X_mag*(0:length(X_mag)-1)')/sum(X_mag)
+    C = X_mag*(0:length(X_mag)-1)'/sum(X_mag);
     
     %w = [1:length(x)].*fs/length(x); %center frequency of each bin
 
--- main.m
+++ main.m
@@ -1,26 +1,26 @@
 deviceReader = audioDeviceReader;
 %deviceReader = dsp.AudioFileReader('Dude.wav');
 deviceWriter = audioDeviceWriter('SampleRate',deviceReader.SampleRate);
-deviceReader.SamplesPerFrame = 64;
+deviceReader.SamplesPerFrame = 1024;
 
 fs = deviceReader.SampleRate;
 
 delay = Delay();
 disp('Begin Signal Input...')
-audioTestBench(delay);
-% tic
-% while toc<50
-%    
-%     mySignal = deviceReader();
-%     %myProcessedSignal = process(delay, mySignal);
-%     deviceWriter(myProcessedSignal);
-%     
-%     %C = centroid(mySignal, deviceReader.SampleRate);
-%     %delay.DelayTime = C/fs; % Adaptive part with some mapping 
-%     
-%     %disp(C);
-%     
-% end
+% audioTestBench(delay);
+tic
+while toc<50
+   
+    mySignal = deviceReader();
+    myProcessedSignal = process(delay, mySignal);
+    deviceWriter(myProcessedSignal);
+    
+    C = centroid(mySignal');
+    delay.DelayTime = C/100; % Adaptive part with some mapping 
+    
+   % disp(C);
+    
+end
 
 release(deviceReader)
 release(deviceWriter)
