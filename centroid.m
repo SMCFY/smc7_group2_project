38fc0a3f8df9c6cfb8e85fe1340b9731ead1e978
NikolajAndersson <nikolajandersson@gmail.com>
2016-10-19 12:53:42 +0200

    just needed to check something

--- centroid.m
+++ centroid.m
@@ -6,8 +6,8 @@ X_mag = abs(X); %magnitude spectrum
 w = [1:length(x)].*fs/length(x); %center frequency of each bin
 
 
-    
-C = sum(w(1:length(w)/2).*X_mag(1:length(X_mag)/2))/sum(X_mag(1:length(X_mag)/2));   
+
+C = sum(w(1:length(w)/2).*X_mag(1:length(X_mag)/2))/sum(X_mag(1:length(X_mag)/2));
 
 end
 
