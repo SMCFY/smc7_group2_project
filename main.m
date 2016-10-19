53b662199b2f0c5bbf91b59854f6ef13c5995375
NikolajAndersson <nikolajandersson@gmail.com>
2016-10-19 15:46:32 +0200

    it works

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
-%deviceReader = audioDeviceReader;
-deviceReader = dsp.AudioFileReader('Dude.wav');
+deviceReader = audioDeviceReader;
+%deviceReader = dsp.AudioFileReader('Dude.wav');
 deviceWriter = audioDeviceWriter('SampleRate',deviceReader.SampleRate);
 deviceReader.SamplesPerFrame = 64;
 
@@ -8,19 +8,19 @@ fs = deviceReader.SampleRate;
 delay = Delay();
 disp('Begin Signal Input...')
 audioTestBench(delay);
-tic
-while toc<50
-   
-    mySignal = deviceReader();
-    myProcessedSignal = process(delay, mySignal);
-    deviceWriter(myProcessedSignal);
-    
-  %  C = centroid(mySignal, deviceReader.SampleRate);
-  %  delay.DelayTime = C/fs; % Adaptive part with some mapping 
-    
-    %disp(C);
-    
-end
+% tic
+% while toc<50
+%    
+%     mySignal = deviceReader();
+%     %myProcessedSignal = process(delay, mySignal);
+%     deviceWriter(myProcessedSignal);
+%     
+%     %C = centroid(mySignal, deviceReader.SampleRate);
+%     %delay.DelayTime = C/fs; % Adaptive part with some mapping 
+%     
+%     %disp(C);
+%     
+% end
 
 release(deviceReader)
 release(deviceWriter)
