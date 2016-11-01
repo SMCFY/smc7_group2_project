aa925246bb33c9575f71995bdd86d05a62481f90
NikolajAndersson <nikolajandersson@gmail.com>
2016-11-01 16:27:51 +0100

    tried to fix merge conflict

--- centroid.m
+++ centroid.m
@@ -1,13 +1,14 @@
-function C = centroid(x, fs)
+function C = centroid(x)
 %SPECTRAL CENTROID calculates the spectral centorid of a discrete signal
 
-X = fft(x); %fas fourier transform
-X_mag = abs(X); %magnitude spectrum
-w = [1:length(x)].*fs/length(x); %center frequency of each bin
+    X = fft(x); %fast fourier transform
+    X_mag = abs(X); %magnitude spectrum
+    X_mag = X_mag(1:length(X_mag)/2); %cropping the n egative region
+    w = [0:length(X_mag)-1] %vector of bins where 0 corresponds to DC component which needs to be excluded
 
-
-
-C = sum(w(1:length(w)/2).*X_mag(1:length(X_mag)/2))/sum(X_mag(1:length(X_mag)/2));
+    C = X_mag * w'/sum(X_mag); % centroid FUCK YEAH
+    
+    %C_hz = C * fs/length(w) 
 
 end
 
