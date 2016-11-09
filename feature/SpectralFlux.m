3200e32d5458e754506d9b636ab9289fa96ae77d
NikolajAndersson <nikolajandersson@gmail.com>
2016-11-09 16:44:48 +0100

    zeros out negative SF values

--- feature/SpectralFlux.m
+++ feature/SpectralFlux.m
@@ -18,9 +18,11 @@ for (i=1:numOfFrames)
     FFT = FFT / max(FFT); % normalization
     if (i>1)
         F(i) = sum((FFT-FFTprev).^2); % spectral difference (NOTE! x^2)
-%       if F(i) < 0
-%           F(i) = 0
-%       end
+      if F(i) < 0
+          F(i) = 0;
+      else
+        F(i) = F(i)^2;
+      end
     else
         F(i) = 0; % the first value of the SF vector
     end
