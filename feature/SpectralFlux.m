738cce406c8cbe3fdfb8585badc48a09d36389a2
NikolajAndersson <nikolajandersson@gmail.com>
2016-11-09 14:15:24 +0100

    added Spectral flux

---
+++ feature/SpectralFlux.m
@@ -0,0 +1,24 @@
+
+function F = SpectralFlux(signal,windowLength, step, fs)
+% SPECTRALFLUX 
+% https://se.mathworks.com/matlabcentral/fileexchange/19236-some-basic-audio-features    
+signal = signal / max(abs(signal));
+curPos = 1;
+L = length(signal);
+numOfFrames = floor((L-windowLength)/step) + 1;
+H = hamming(windowLength);
+m = [0:windowLength-1]';
+F = zeros(numOfFrames,1);
+for (i=1:numOfFrames)
+    window = H.*(signal(curPos:curPos+windowLength-1));    
+    FFT = (abs(fft(window,2*windowLength)));
+    FFT = FFT(1:windowLength);        
+    FFT = FFT / max(FFT);
+    if (i>1)
+        F(i) = sum((FFT-FFTprev).^2);
+    else
+        F(i) = 0;
+    end
+    curPos = curPos + step;
+    FFTprev = FFT;
+end
