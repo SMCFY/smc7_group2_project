bb725981b323652bb2eeed28baa2132bac90bf09
NikolajAndersson <nikolajandersson@gmail.com>
2016-11-09 11:20:18 +0100

    added comments for reverse

--- effects/reverse.m
+++ effects/reverse.m
@@ -1,6 +1,7 @@
 function [out] = reverse(x)
     ham = hamming(length(x));
     x = x.*ham;
-    out = flip(x);
-    
+    out = flip(x); % does not work
+    % Create a buffer of delayed frames, read from the buffer in reverse
+    % input : ->->->->->,, reverse: <-<-<-<-, 
 end
