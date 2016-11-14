5daf4d862685a3f79ce356034331b8ab47ab8300
jholfelt <jholfelt@gmail.com>
2016-11-14 13:52:39 +0100

    i don't know

--- main.m
+++ main.m
@@ -29,10 +29,10 @@ while toc<50
     % Adaptive part with some mapping 
     % use the interpolator to smooth out centroid-to-delay mapping
     [delay.DelayTime count C_old C_new delta] = interpolator(delay.DelayTime,mySignal, C_old,C_new, count, delta);
-
+    
     % keeping track of all the calculated delay values (for testing purposes)
     dtime = [dtime delay.DelayTime];
-
+    
     %disp(C);
     
 end
