04ad41caae351d72909c90451ab0993e22939bd0
NikolajAndersson <nikolajandersson@gmail.com>
2016-11-16 13:29:46 +0100

    added a runtime indicator + onset test

--- main.m
+++ main.m
@@ -1,28 +1,32 @@
 deviceReader = audioDeviceReader;
 %deviceReader = dsp.AudioFileReader('Dude.wav');
 deviceWriter = audioDeviceWriter('SampleRate',deviceReader.SampleRate);
-deviceReader.SamplesPerFrame = 1024;
+deviceReader.SamplesPerFrame = 256;
 
 delay = Delay2();
 disp('Begin Signal Input...')
 
 XmagPrev = 0;
-threshold = 50;
+threshold = 12;
+runTime = 5; % run time in seconds
 bufferSize = deviceReader.SamplesPerFrame;
 fs = deviceReader.SampleRate;
-
+onsetBuffer = zeros(runTime*fs,1);
+count = 0;
 tic
-while toc<30
+while toc<runTime
    
     mySignal = deviceReader();
-    myProcessedSignal = process(delay, mySignal);
-    deviceWriter(myProcessedSignal);
+    %myProcessedSignal = process(delay, mySignal);
+   % deviceWriter(myProcessedSignal);
     
     [onset, XmagPrev] = detectOnset(mySignal, threshold, XmagPrev);
     if onset == 1
     	disp('ONSET');
+        onsetBuffer(count*bufferSize,1) = 1;
+        
     end
-    
+    count = count + 1;
     %disp(estimateTempo(10, fs, bufferSize, mySignal, threshold, XmagPrev));
     
     %C = centroid(mySignal');
@@ -31,6 +35,13 @@ while toc<30
     %disp(C);
     
 end
+%amountOfOnsets = sum(onsetBuffer);
+onsetBuffer = onsetBuffer(1:count*bufferSize);
+t = sprintf('Amount of onsets detected: %d', sum(onsetBuffer));
+disp(t)
+%runTime = (count*bufferSize)/fs
+t = sprintf('run time in seconds: %1f', (count*bufferSize)/fs);
+disp(t)
 
 release(deviceReader)
 release(deviceWriter)
