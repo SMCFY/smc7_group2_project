19156d8a1bce7728eb57381e617faa9f6ec20fc2
gericsapo <geri@MacBook-Pro.local>
2016-11-20 16:47:06 +0100

    onset detector shows random noise, which needs debugging...

--- feature/detectOnset.m
+++ feature/detectOnset.m
@@ -1,23 +1,28 @@
 
-function [SF, onset, XmagPrev] = detectOnset(signal, threshold, XmagPrev)
+function [onset, XmagPrev, SF] = detectOnset(signal, threshold, XmagPrev)
 % ONSET DETECTION 
-%   detects musical onsets by transient regions based on spectral flux
+%   detects musical onsets by transient regions in signal based on spectral flux
 % 
 % ARGUMENTS:
 %   signal - the windowed signal (buffer)
 %   threshold - the threshold of spectral flux for onset detection
 %   XmagPrev - previously returned magnitude spectrum
 %   
+% OUTPUT:
+%   onset - onset
+%   XmagPrev - spectrum buffer
+%   SF - spectral flux
 
 signal = signal / max(abs(signal)); % normalization of the signal   
 Xmag = abs(fft(signal,2*length(signal))); %magnitude spectrum
 Xmag = Xmag(1:length(signal)); % consider spectrum until nyquist freq     
 Xmag = Xmag / max(Xmag); % normalization of the spectrum
 
-
 if (XmagPrev>0)
-
-    SF = sum(((Xmag-XmagPrev) + abs(Xmag-XmagPrev)) / 2); % rectified spectral flux 
+    
+    specDiff = (Xmag-XmagPrev + abs(Xmag-XmagPrev))/2; % rectified spectral difference
+    SF = sum(specDiff); % spectral flux
+    SF = SF^10; % accentuation
 
 else
     SF = 0; % the first output
--- main.m
+++ main.m
@@ -1,20 +1,23 @@
 deviceReader = audioDeviceReader;
-%deviceReader = dsp.AudioFileReader('Dude.wav');
+%deviceReader = dsp.AudioFileReader('/Users/Geri/Documents/MATLAB/sp/sound_files/Gregorythme_SOOML.mp3');
 deviceWriter = audioDeviceWriter('SampleRate',deviceReader.SampleRate);
 deviceReader.SamplesPerFrame = 256;
 
 delay = Delay2();
 disp('Begin Signal Input...')
-
-XmagPrev = 0;
-threshold = 40;
-runTime = 5; % run time in seconds
-bufferSize = deviceReader.SamplesPerFrame;
 fs = deviceReader.SampleRate;
-onsetBuffer = zeros(runTime*fs,1);
-count = 0;
+%runTime = 5; % run time in seconds
+%bufferSize = deviceReader.SamplesPerFrame;
+%count = 0;
+%onsetBuffer = zeros(runTime*fs,1);
+
+% ONSET TEST PARAMS -----------
+XmagPrev = zeros(1,256);
+threshold = 700;
 noveltyC = [];
 onsetV = [];
+% ------------------------------
+
 tic
 while toc<runTime
    
@@ -22,28 +25,31 @@ while toc<runTime
     %myProcessedSignal = process(delay, mySignal);
    % deviceWriter(myProcessedSignal);
     
-    [SF, onset, XmagPrev] = detectOnset(mySignal, threshold, XmagPrev);
+    [onset, XmagPrev, SF] = detectOnset(mySignal, threshold, XmagPrev);
     if onset == 1
     	disp('ONSET');
-        onsetBuffer(count*bufferSize,1) = 1;
+        %onsetBuffer(count*bufferSize,1) = 1;
         
     end
-    count = count + 1;
+    %count = count + 1;
     
     noveltyC = [noveltyC , SF];
     onsetV = [onsetV, onset];
     
 end
-%amountOfOnsets = sum(onsetBuffer);
-onsetBuffer = onsetBuffer(1:count*bufferSize);
-t = sprintf('Amount of onsets detected: %d', sum(onsetBuffer));
-disp(t)
-%runTime = (count*bufferSize)/fs
-t = sprintf('run time in seconds: %1f', (count*bufferSize)/fs);
-disp(t)
 
+%amountOfOnsets = sum(onsetBuffer);
+%onsetBuffer = onsetBuffer(1:count*bufferSize);
+%t = sprintf('Amount of onsets detected: %d', sum(onsetBuffer));
+%disp(t)
+%runTime = (count*bufferSize)/fs
+%t = sprintf('run time in seconds: %1f', (count*bufferSize)/fs);
+%disp(t)
+
+% ONSET PLOT ---------
 plot(noveltyC); hold on;
-plot(onsetV*max(noveltyC));
+plot(onsetV * threshold);
+% ---------------------
 
 release(deviceReader)
 release(deviceWriter)
