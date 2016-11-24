983f839d89eaa3a503605f7427029b5f517a1585
NikolajAndersson <nikolajandersson@gmail.com>
2016-11-24 23:34:57 +0100

    Fixed vibrato and cleaned up delay class Also added a mapping structure

--- effects/vibrato.m
+++ effects/vibrato.m
@@ -1,5 +1,6 @@
 % Vibrato 
 % taken from this website http://users.cs.cf.ac.uk/Dave.Marshall/CM0268/PDF/10_CM0268_Audio_FX.pdf
+% Example from DAFX book by Udo
 function [y, buffer, bufferIndex, sineP] = vibrato(x, fs, modfreq, width, buffer,bufferIndex, sineP)
 % Modfreq in Khz, Width = 0.0008; % 0.8 Milliseconds
 y = zeros(size(x));
@@ -8,8 +9,8 @@ delay = width;
 M = modfreq/fs; % modulation frequency in # samples
 
 for i=1:size(x,1)
-   buffer(wIndex, :) = x(i,:); 
-
+   buffer(wIndex,:) = x(i,:);
+   
    MOD=sin(M*2*pi*sineP);
    
    sineP = sineP + 1;
@@ -17,25 +18,29 @@ for i=1:size(x,1)
    if sineP > 192001
        sineP = 1;
    end
-   
-   ZEIGER=1+delay+width*MOD;
-   n=floor(ZEIGER);
-   frac=ZEIGER-n;
-   rIndex = wIndex - n;
+   % Delay tap
+   TAP=1+delay+width*MOD;
+   n=floor(TAP);
+   %frac = TAP-n;
+   rIndex = floor(wIndex - n);
    
    if rIndex <= 0
        rIndex = rIndex + 192001;
    end
    
    %---Linear Interpolation-----------------------------
-   y(i,:)=buffer(rIndex+1)*frac+buffer(rIndex)*(1-frac); 
-   %y(i,2)=buffer(rIndex+1)*frac+buffer(rIndex)*(1-frac); 
+   %y(i,:) = buffer(rIndex)*frac+buffer(rIndex)*(1-frac); 
+   y(i,:) = buffer(rIndex);
+  % y(i,2)=buffer(rIndex+1)*frac+buffer(rIndex)*(1-frac); 
+   
+   %---Spline Interpolation------------------------------- 
+%   y(i,1)=buffer(rIndex+1)*frac^3/6+buffer(rIndex)*((1+frac)^3-4*frac^3)/6 +buffer(rIndex-1)*((2-frac)^3-4*(1-frac)^3)/6 +buffer(rIndex-2)*(1-frac)^3/6;
    
    wIndex = wIndex + 1;
    if wIndex > 192001
        wIndex =  1;
    end
-
+end
 bufferIndex = wIndex;
 
 end 
