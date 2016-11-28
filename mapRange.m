9a845f3a8c1bbf03f079ce2eb59e8d3b8da40037
jholfelt <jholfelt@gmail.com>
2016-11-28 10:40:56 +0100

    modified mapping range function

--- mapRange.m
+++ mapRange.m
@@ -1,9 +1,12 @@
 function output = mapRange( outMax, outMin, inMax, inMin, input )
+%This function maps an input range of numbers to another output range
 
+%Clip to inMax
 if input > inMax
     input = inMax;
 end 
 
+%Calculations. Calculating slope first for simplification
 slope = (outMax - outMin) / (inMax - inMin);
 output = outMin + slope * (input - inMin);
     
