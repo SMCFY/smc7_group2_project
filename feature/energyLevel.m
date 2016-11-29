e2e9365652e93748431333c025815c602e14a1b3
jholfelt <jholfelt@gmail.com>
2016-11-29 15:47:38 +0100

    normalized energyLevel to buffersize

--- feature/energyLevel.m
+++ feature/energyLevel.m
@@ -1,14 +1,15 @@
 function E = energyLevel(x,A)
 
-%code based on work by
-%   Author: Nabin Sharma
-%   Date: 2009/03/15
+    %code based on work by
+    %   Author: Nabin Sharma
+    %   Date: 2009/03/15
 
-% generate the window
-ham = hamming(length(x));
-window = A.*ham';
+    % generate the window
+    ham = hamming(length(x));
+    window = A.*ham';
 
-% enery calculation
-x2 = x.^2;
-E = wconv(x2,window,length(x));
+    % enery calculation
+    x2 = x.^2;
+    E = sum(wconv(x2,window,length(x)))/length(x);
+    
 end
--- feature/wconv.m
+++ feature/wconv.m
@@ -1,15 +1,15 @@
 function y = wconv( x, A, L )
-%code based on work by
-%   Author: Nabin Sharma
-%   Date: 2009/03/15
-% generate the window
-ham = hamming(length(x));
-window = A.*ham';
+    %code based on work by
+    %   Author: Nabin Sharma
+    %   Date: 2009/03/15
+    % generate the window
+    ham = hamming(length(x));
+    window = A.*ham';
 
-% perform the convolution using FFT
-NFFT = 2^(nextpow2(length(x)+L));
-X = fft(x,NFFT); W = fft(window,NFFT);
-Y = X.*W;
-y = ifft(Y,NFFT);
+    % perform the convolution using FFT
+    NFFT = 2^(nextpow2(length(x)+L));
+    X = fft(x,NFFT); W = fft(window,NFFT);
+    Y = X.*W;
+    y = ifft(Y,NFFT);
 end
 
