362914be87dc6bbb41ef4d1d56e5f80c1d128f45
gericsapo <gericsapo@gmail.com>
2016-12-08 15:31:49 +0100

    updated comments and variable names

--- feature/localizeOnset.m
+++ feature/localizeOnset.m
@@ -1,18 +1,19 @@
-function [onsetDev, onsetInterval, curPos] = localizeOnset(noveltyC, durationInBuffers, threshold, temporalThreshold, onsetInterval, curPos, onsetDev, noveltyCLength)
-% INTER-ONSET INTERVAL DEVIATION
-%   localizing onsets in the specified time period and calculating the inter-onset interval deviation
-%	from the avarage onset interval
+function [avgOnset, onsetInterval] = localizeOnset(noveltyC, durationInBuffers, threshold, temporalThreshold, onsetInterval, avgOnset, noveltyCLength)
+% AVERAGE INTER-ONSET INTERVAL
+%   localizing onsets in the specified time period and calculating the average inter-onset interval
 %
 % ARGUMENTS:
 % 	noveltyC - novelty curve
-%   durationInBuffers - considered period for onsets
+%   	durationInBuffers - considered period for onsets
 %	threshold - absolute treshold in SF for onset detection
 %	temporalThreshold - temporal threshold in buffers for onset detection 
-%	curPos - current position in reading the novelty curve
+%	onsetInterval - IOI
+%	avgOnset - average IOI
+%	noveltyCLength - window size
 %   
 % OUTPUT:
-%   onsetDev - IOI deviation
-%	curPos - window position on the novelty curve
+%	onsetInterval - IOI between the last 2 onset
+%   	avgOnset - average IOI
 
 if sum(noveltyC) > 0            %if (durationInBuffers < length(noveltyC))
     onsetVector = noveltyC(1:noveltyCLength); %windowed novelty curve
@@ -21,16 +22,17 @@ if sum(noveltyC) > 0            %if (durationInBuffers < length(noveltyC))
     onsetLoc = zeros(1,length(onsetVector));
     for i=1:length(onsetVector)
         if (onsetVector(i)>threshold && temporalThreshold<0)
-            if (sum(sign(onsetLoc)==1))
-              onsetLoc(i) = 1; % store elapsed time in terms of buffers since last onset, when new onset is recorded
+            if (sum(sign(onsetLoc)==1)) %solving corner case if only one onset is recorded in the vector
+              onsetLoc(i) = 1; 
               onsetInterval = 0; % initializing onset interval counter
-            else
+            else % needs 2 or more onset in the vector in order to calcule a valid IOI value
               onsetLoc(i) = onsetInterval; % store elapsed time in terms of buffers since last onset, when new onset is recorded
             end
-            onsetDev = sum(onsetLoc)/sum(sign(onsetLoc)); % IOI deviation
-	    onsetDev = 1/onsetDev;
+	    
+            avgOnset = sum(onsetLoc)/sum(sign(onsetLoc)); % average IOI
+	    avgOnset = 1/avgOnset; % inverting value
             
-			temporalThreshold = 20; % initializing temporal threshold
+	    temporalThreshold = 20; % initializing temporal threshold
             onsetInterval = 0; % initializing onset interval counter
         else
             onsetLoc(i) = 0;
@@ -39,12 +41,10 @@ if sum(noveltyC) > 0            %if (durationInBuffers < length(noveltyC))
             onsetInterval = onsetInterval +1;
         end
     end
-
-    %curPos = curPos +1; % moving the window
     
 else
     
-    onsetDev = 0;
+    avgOnset = 0;
     
 end
 
