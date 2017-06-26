51f36199e4197bc528bcec28921bcecfcda7b9b7
Nikolaj Andersson <nikolajandersson@gmail.com>
2017-06-26 15:21:14 +0200

    added feature extraction class

---
+++ refactoring/FeatureExtraction.m
@@ -0,0 +1,12 @@
+classdef FeatureExtraction
+    %Collection of feature extraction algorithms
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
